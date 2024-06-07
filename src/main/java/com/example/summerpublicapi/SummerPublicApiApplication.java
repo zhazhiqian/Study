2d1a89e70b9a2317f98a2803fca56bf5073f7432
zhazhiqian <zhazhiqian@kayak.com.cn>
2024-06-08 01:42:24 +0800

    1

---
+++ src/main/java/com/example/summerpublicapi/SummerPublicApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.summerpublicapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SummerPublicApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SummerPublicApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/summerpublicapi/SummerPublicApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.summerpublicapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SummerPublicApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
